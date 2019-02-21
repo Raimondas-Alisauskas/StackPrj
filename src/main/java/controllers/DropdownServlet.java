b9870efd5c243cda1853327a81c1ccc639cb884b
RaimondasA <ra.alisa.123@gmail.com>
2019-02-21 15:05:27 +0200

    data from database to dropdown menu added

--- src/main/java/controllers/DropdownServlet.java
+++ src/main/java/controllers/DropdownServlet.java
@@ -2,37 +2,33 @@ package controllers;
 
 //import model.UserBean;
 
+import models.DocTagDAL;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.sql.ResultSet;
+import java.util.List;
 
-/**
- * Servlet implementation class controllers.LoginServlet
- */
-public class PaginationServlet extends HttpServlet {
+@WebServlet("")
+public class DropdownServlet extends HttpServlet {
 
-
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, java.io.IOException {
 
         try {
 
+            DropdownBL dropdownBL = new DropdownBL();
 
-            String tag = request.getParameter("un");
-            String recordsNumb = request.getParameter("pw");
-
-
-
-//            UserBean user = new UserBean();
-//            user.setUserName(request.getParameter("un"));
-//            user.setPassword(request.getParameter("pw"));
-//
-            PaginationBL paginationBL = new PaginationBL();
-
-            ResultSet resultSet = paginationBL.getLimitedResult();
+            List<DocTagDAL> tagList = dropdownBL.getLimitedResult();
+            request.setAttribute("tagList", tagList);
+            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+            rd.forward(request, response);
+            System.out.println(tagList);
 
 //
 //            if (user.isValid())
@@ -43,8 +39,7 @@ public class PaginationServlet extends HttpServlet {
 //                response.sendRedirect("jsp/userLogged.jsp"); //logged-in page
 //            }
 //
-//            else
-//                response.sendRedirect("jsp/invalidLogin.jsp"); //error page
+
 
         } catch (Throwable theException)
         {
