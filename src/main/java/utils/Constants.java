d5cff25f396e1ffd916ce70d9c85489748e78062
Artiom171 <artiom201097@gmail.com>
2019-03-01 12:25:41 +0200

    Pulling to my branch

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class Constants {
-    public static final String DB_CONNECTION_STR ="jdbc:sqlite:/home/namai/Desktop/CodeBakers/Mokymasis/StackOverflowProject/StackPrj4/database/stackoverflow.db";
+    public static final String DB_CONNECTION_STR ="jdbc:sqlite:C://StackPrj/database/stackoverflow.db";
     public static final int SHOW_NUMB_OF_TITLES =8;
     public static final int SHOW_NUMB_OF_TAGS =5;
 }
