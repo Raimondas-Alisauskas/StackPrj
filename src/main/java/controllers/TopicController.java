6a5c7552204c4a5cbea2d427651fd9c6dd4d2b88
justinasin <justinasin@gmail.com>
2019-03-04 15:13:59 +0200

    Some additional changes

--- src/main/java/controllers/TopicController.java
+++ src/main/java/controllers/TopicController.java
@@ -18,18 +18,20 @@ public class TopicController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        TopicService topicService = new TopicService();
+        TopicService dropdown = new TopicService();
+
         String tagId = req.getParameter("tagId");
 
         String search_field = req.getParameter("search_field");
 
         String pageNumStr = req.getParameter("pageNum");
 
-        TopicService topicService = new TopicService();
+
         TopicDTO topicDTO = topicService.getTopics(tagId,search_field,pageNumStr);
 
         req.setAttribute("topicDTO", topicDTO);
 
-        TopicService dropdown = new TopicService();
         List<DropdownBean> tagList = dropdown.getDropdown();
 
         req.setAttribute("tagList", tagList);
